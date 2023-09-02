37cbb01e0eb4a7e02c9431bb5d004cd38d4cda05
Emmylong1 <emmanuelibok505@gmail.com>
2023-09-02 08:58:49 +0000

    add content to the code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Emmanuelibok" }');
+    res.send('{ "response": "Hello Emmanuel it's great to connect with you. I've been working on githubworkflows project, and I thought it would be helpful to get your input on" }');
 });
 
 app.get('/will', function (req, res) {
